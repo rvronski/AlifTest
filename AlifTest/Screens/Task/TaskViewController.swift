4ea42a79387e6719d3f0fcb055979784a6b59f03
rvronski <rvronski@gmail.com>
2023-04-26 04:01:36 +0500

    create TaskVC

---
+++ AlifTest/Screens/Task/TaskViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TaskViewController.swift
+//  AlifTest
+//
+//  Created by ROMAN VRONSKY on 26.04.2023.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
