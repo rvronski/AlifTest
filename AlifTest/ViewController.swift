fbb93409049d70e9b8cbdf20526f1965eee825b4
rvronski <rvronski@gmail.com>
2023-04-26 02:11:07 +0500

    Initial Commit

---
+++ AlifTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlifTest
+//
+//  Created by ROMAN VRONSKY on 26.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
